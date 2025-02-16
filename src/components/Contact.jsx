1c700fc0ad626b0d5ef78f20ca8fd44dfa402e5e
Vivek Narayan <ervnarayan@gmail.com>
2025-02-16 13:30:12 +0530

    Contact Form submit bug resolved

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,14 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { ClipLoader } from 'react-spinners';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const Contact = () => {
+
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+ 
+    try{
+      const {data} = await axios.post('http://localhost:4000/api/sendmail', {name, email, message}, {withCredentials: true, headers: {"Content-Type":"application/json"}});
+      setEmail("");
+      setName("");  
+      setMessage("");
+      setLoading(false);
+      toast.success(data.message);
+    }catch(error){
+      setLoading(false);
+      toast.error(error.response.data.message);
+    }
+
+  }
+
   return (
     <section className='contact'>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>CONTACT US</h1>
         <div>
           <label>Name</label>
@@ -16,11 +38,11 @@ const Contact = () => {
         </div>
         <div>
           <label>Email</label>
-          <input type='email' value={email} onChange={(e)=>setEmail(e.target.email)}/>
+          <input type='text' value={email} onChange={(e)=>setEmail(e.target.value)}/>
         </div>
         <div>
           <label>Message</label>
-          <input type='text'value={message} onChange={(e)=>setMessage(e.target.message)}/>
+          <input type='text'value={message} onChange={(e)=>setMessage(e.target.value)}/>
         </div>
         <button type='submit' disabled={loading} style={{display:"flex", justifyContent: "center", alignItems: "center", gap: "15px"}} > {loading && <ClipLoader size={20} color="white" />}Send Message</button>
       </form>
